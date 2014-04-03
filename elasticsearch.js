892fc0a3a19c8f8b182a3b7063db83f47e43b027
Adrian Rossouw <adrian@daemon.co.za>
2014-04-03 06:40:10 +0200

    change punctuation, add elastic.js

--- elasticsearch.js
+++ elasticsearch.js
@@ -1,119 +1,119 @@
 
-var pluginName    = 'search'
+var pluginName    = 'search';
 
-var assert        = require('assert')
-var elasticsearch = require('elasticsearch')
+var assert        = require('assert');
+var elasticsearch = require('elasticsearch');
 
 function search(options) {
 
-  var seneca = this
+    var seneca = this;
 
-  options = options || {}
+    options = options || {};
 
-  var connectionOptions = options.connection || {
-    host: 'localhost:9200',
-    sniffOnStart: true,
-    sniffInterval: 300000,
-    log: 'error'
-  }
+    var connectionOptions = options.connection || {
+        host: 'localhost:9200',
+        sniffOnStart: true,
+        sniffInterval: 300000,
+        log: 'error'
+    };
 
-  var esClient = new elasticsearch.Client(connectionOptions)
+    var esClient = new elasticsearch.Client(connectionOptions);
 
-  var indexes = {}
+    var indexes = {};
 
-  seneca.add({role: pluginName, cmd: 'create-index'}, function(args, callback) {
-    var indexName = args.index
+    seneca.add({role: pluginName, cmd: 'create-index'}, function(args, callback) {
+        var indexName = args.index;
 
-    var fields = []
+        var fields = [];
 
-    if(args.fields) {
-      for(var i = 0 ; i < args.fields.length ; i++) {
-        var field = args.fields[i]
-        var suffix = ''
-        if(Number(field.priority) !== NaN && field.priority > 1) {
-          suffix = '^'+field.priority
+        if (args.fields) {
+            for(var i = 0 ; i < args.fields.length ; i++) {
+                var field = args.fields[i];
+                var suffix = '';
+                if(!isNaN(Number(field.priority)) && field.priority > 1) {
+                    suffix = '^'+field.priority;
+                }
+
+                fields.push(field.name + suffix);
+            }
         }
 
-        fields.push(field.name + suffix)
-      }
-    }
+        indexes[indexName] = {
+            fields: fields.length > 0 ? fields : ['id^3', 'name^2', '*']
+        };
 
-    indexes[indexName] = {
-      fields: fields.length > 0 ? fields : ['id^3', 'name^2', '*']
-    }
-
-    esClient.indices.exists({index: indexName}, function(err, exists) {
-      if(err || exists) {
-        return callback(err)
-      } else {
-        esClient.indices.create({index: indexName}, function(err) {
-          callback(err)
-        })
-      }
-    })
+        esClient.indices.exists({index: indexName}, function(err, exists) {
+            if(err || exists) {
+                return callback(err);
+            } else {
+                esClient.indices.create({index: indexName}, function(err) {
+                    callback(err);
+                });
+            }
+        });
 
 
-  })
-
-  seneca.add({role: pluginName, cmd: 'save'}, function(args, callback) {
-
-    assert.ok(args.data, 'missing args.data')
-    var dataType = args.type || args.data.entity$
-    assert.ok(dataType, 'expected either "type" or "data.entity$" to deduce the entity type')
-
-    esClient.index({
-      index: args.index,
-      type: dataType,
-      id: args.data.id,
-      refresh: options.refreshOnSave,
-      body: args.data
-    }, function (err, resp) {
-      if(err) {
-        console.error(err)
-      }
-      callback(err)
-    })
-
-  })
-
-  seneca.add({role: pluginName, cmd: 'delete'}, function(args, callback) {
-    callback(undefined)
-  })
-
-  seneca.add({role: pluginName, cmd: 'search'}, function(args, callback) {
-
-    var indexName = args.index
-    var fields = indexes[indexName] ? indexes[indexName].fields : ['*']
-
-    var searchParams = {
-      index: indexName,
-      body: {
-        query: {
-          multi_match : {
-            query        : args.query,
-            type         : 'best_fields',
-            fields       : fields,
-            tie_breaker  : 0.3
-          }
-        }
-      }
-    }
-
-    if(args.type) {
-      search.type = args.type
-    }
-
-    esClient.search(searchParams).then(function (resp) {
-      callback(undefined, resp.hits.hits)
-    }, function (err) {
-      callback(err, undefined)
     });
-  })
 
-  return {
-    name: pluginName,
-    native: esClient
-  }
+    seneca.add({role: pluginName, cmd: 'save'}, function(args, callback) {
+
+        assert.ok(args.data, 'missing args.data');
+        var dataType = args.type || args.data.entity$;
+        assert.ok(dataType, 'expected either "type" or "data.entity$" to deduce the entity type');
+
+        esClient.index({
+            index: args.index,
+            type: dataType,
+            id: args.data.id,
+            refresh: options.refreshOnSave,
+            body: args.data
+        }, function (err, resp) {
+            if(err) {
+                console.error(err);
+            }
+            callback(err);
+        });
+
+    });
+
+    seneca.add({role: pluginName, cmd: 'delete'}, function(args, callback) {
+        callback(undefined);
+    });
+
+    seneca.add({role: pluginName, cmd: 'search'}, function(args, callback) {
+
+        var indexName = args.index;
+        var fields = indexes[indexName] ? indexes[indexName].fields : ['*'];
+
+        var searchParams = {
+            index: indexName,
+            body: {
+                query: {
+                    multi_match : {
+                        query        : args.query,
+                        type         : 'best_fields',
+                        fields       : fields,
+                        tie_breaker  : 0.3
+                    }
+                }
+            }
+        };
+
+        if(args.type) {
+            search.type = args.type;
+        }
+
+        esClient.search(searchParams).then(function (resp) {
+            callback(undefined, resp.hits.hits);
+        }, function (err) {
+            callback(err, undefined);
+        });
+    });
+
+    return {
+        name: pluginName,
+        native: esClient
+    };
 }
 
-module.exports = search
+module.exports = search;
