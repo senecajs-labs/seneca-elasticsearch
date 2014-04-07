6dbea10dc207333794cd281aa24bdbd379fdf806
Adrian Rossouw <adrian@daemon.co.za>
2014-04-07 03:10:32 +0200

    tidy up act calling logic a bit.

--- elasticsearch.js
+++ elasticsearch.js
@@ -27,70 +27,77 @@ function search(options, register) {
 
   var esClient = new elasticsearch.Client(connectionOptions);
 
+  /**
+  * Seneca bindings.
+  *
+  * We compose what needs to happen during the events
+  * using async.seq, which nests the calls the functions
+  * in order, passing the same context to all of them.
+  */
+
+  // index events
   seneca.add({role: pluginName, cmd: 'create-index'}, ensureIndex);
+
   seneca.add({role: pluginName, cmd: 'has-index'}, hasIndex);
+
   seneca.add({role: pluginName, cmd: 'delete-index'},
     async.seq(ensureIndex, deleteIndex));
 
+  // data events
   seneca.add({role: pluginName, cmd: 'save'},
-    async.seq(ensureIndex, populateRequest, populateBody, saveRecord));
+    async.seq(ensureIndex, populateRequest,
+      populateBody, saveRecord));
 
   seneca.add({role: pluginName, cmd: 'load'},
     async.seq(ensureIndex, populateRequest, loadRecord));
 
-  seneca.add({role: pluginName, cmd: 'search'}, async.seq(
-      ensureIndex, populateRequest, populateSearch,
-      populateSearchBody, doSearch));
+  seneca.add({role: pluginName, cmd: 'search'},
+    async.seq(ensureIndex, populateRequest,
+      populateSearch, populateSearchBody, doSearch));
 
   seneca.add({role: pluginName, cmd: 'remove'},
     async.seq(ensureIndex, populateRequest, removeRecord));
 
-  seneca.add({role:'entity',cmd:'save'}, function(args, cb) {
-    console.log('entity to index:'+args.ent);
+  // entity events
+  seneca.add({role:'entity',cmd:'save'},
+    async.seq(populateCommand, entitySave, entityAct));
 
-    var prior = this.prior.bind(this);
-
-    var data =  args.ent.data$();
-
-    var cmd  = {
-      role  : pluginName,
-      cmd   : 'save',
-      index : connectionOptions.index,
-      type  : data.entity$.name,
-      data  : data
-    };
-
-    seneca.act( cmd, function( err ) {
-      if(err) { return seneca.fail(err); }
-      prior(args, cb);
-    });
-
-  });
-
-  seneca.add({role:'entity',cmd:'remove'}, function(args, cb) {
-    console.log('entity to remove:'+args.ent.id);
-
-    var data =  args.ent.data$();
-    var prior = this.prior.bind(this);
-
-    var cmd  = {
-      role  : pluginName,
-      cmd   : 'remove',
-      index : connectionOptions.index,
-      type  : data.entity$.name,
-      data  : { id: data.id }
-    };
-
-    seneca.act( cmd, function( err ) {
-      if(err) { return seneca.fail(err); }
-      prior(args, cb);
-    });
-  });
+  seneca.add({role:'entity',cmd:'remove'},
+    async.seq(populateCommand, entityRemove, entityAct));
 
   register(null, {
     name: pluginName,
     native: esClient
   });
+
+  /*
+  * Entity management
+  */
+  function entitySave(args, cb) {
+    args.command.cmd = 'save';
+    args.command.data = args.ent.data$();
+
+    // TODO: _.pick only the specified keys
+    cb(null, args);
+  }
+
+  function entityRemove(args, cb) {
+    var prior = this.prior.bind(this);
+
+    args.command.cmd = 'remove';
+    args.command.data = { id: args.ent.id };
+    cb(null, args);
+  }
+
+  function entityAct(args, cb) {
+    assert(args.command, "missing args.command");
+
+    var prior = this.prior.bind(this);
+    seneca.act( args.command, function( err ) {
+      if(err) { return seneca.fail(err); }
+      prior(args, cb);
+    });
+  }
 
   /*
   * Index management.
@@ -109,7 +116,7 @@ function search(options, register) {
 
   // creates the index for us if it doesn't exist.
   function ensureIndex(args, cb) {
-    hasIndex(args, onExists)
+    hasIndex(args, onExists);
 
     function onExists(err, exists) {
       if (err || !exists) {
@@ -142,14 +149,29 @@ function search(options, register) {
   /**
   * Constructing requests.
   */
+
+  function populateCommand(args, cb) {
+    args.entityData = args.ent.data$();
+    args.command = {
+      role  : pluginName,
+      index : connectionOptions.index,
+      type  : args.entityData.entity$.name,
+    }
+
+    cb(null, args);
+  }
+
   function populateBody(args, cb) {
     args.request.body = args.data;
     cb(null, args);
   }
 
   function populateSearch(args, cb) {
-    args.searchRequest = ejs.Request()
-        .query(ejs.MatchAllQuery());
+    var defaultSearch = ejs.Request()
+      .query(ejs.MatchAllQuery());
+
+    args.searchRequest = args.search || defaultSearch;
+
     cb(null, args);
   }
 
@@ -173,7 +195,6 @@ function search(options, register) {
 
     cb(null, args);
   }
-
 
   // ensures callback is called consistently
   function passArgs(args, cb) {
